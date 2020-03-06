d4bec170922f65a37bc00c1f78d1ca253fe7f899
Axis96 <61839359+Axis96@users.noreply.github.com>
2020-03-06 18:41:35 +0100

    Update GestioneBiblioteca.java

--- GestioneProgettoCondiviso/src/it/dstech/progettocondiviso/GestioneBiblioteca.java
+++ GestioneProgettoCondiviso/src/it/dstech/progettocondiviso/GestioneBiblioteca.java
@@ -37,7 +37,35 @@ public class GestioneBiblioteca {
 	}
 	
 	
-	
+	public static void aggiornaLibro() {
+		boolean sceltaCorretta= true;
+		System.out.println("Ecco la lista degli scaffali, quale vuoi selezionare?");
+		stampaScaffali();
+		int scaffale= input.nextInt();
+		input.nextLine();
+		System.out.println("Ecco la lista dei libri in questo scaffale, quale vuoi aggiornare?");
+		stampaLibri();
+		int libro=input.nextInt();
+		input.nextLine();
+		while(sceltaCorretta) {
+			System.out.println("Quale attributo del libro vuoi cambiare? 1.Titolo 2.Autore 3.Genere");
+			int scelta=input.nextInt();
+			if (scelta==1 || scelta==2 || scelta==3) {
+				sceltaCorretta=false;
+				if(scelta==1) {
+					System.out.println("Inserisci il nuovo autore");
+					listaScaffali.get(scaffale).get(libro).setTitolo(input.nextLine());
+				} else if (scelta==2){
+					System.out.println("Inserisci il nuovo titolo");
+					listaScaffali.get(scaffale).get(libro).setAutore(input.nextLine());
+				} else {
+					System.out.println("Inserisci il nuovo genere");
+					listaScaffali.get(scaffale).get(libro).setGenere(input.nextLine());
+					inserisciLibroNelGiustoScaffale;
+				}
+			}
+		}
+	}
 	
 	
 }
